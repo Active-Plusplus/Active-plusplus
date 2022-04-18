9975a672702b77e0b9364b68f899642ebbe6fecf
tylertran11 <tylertran1218@gmail.com>
2022-04-17 22:31:47 -0700

    Initial Commit

---
+++ Active++/View Controllers/ExerciseViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ExerciseViewController.swift
+//  Active++
+//
+//  Created by Tyler  Tran  on 4/17/22.
+//
+
+import UIKit
+import Parse
+
+class ExerciseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func onLogoutButton(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else { return }
+        
+        delegate.window?.rootViewController = loginViewController
+                
+    }
+}
